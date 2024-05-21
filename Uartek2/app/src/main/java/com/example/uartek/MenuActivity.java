84c007ebcf3b38ab8a40072e2e8f4d8a6782015b
Simay AYANOĞLU <128415906+s1m4y2@users.noreply.github.com>
2024-05-21 19:23:51 +0300

    Add files via upload
    ayarlar sayfası ve menü sayfasını düzenledim

---
+++ Uartek2/app/src/main/java/com/example/uartek/MenuActivity.java
@@ -0,0 +1,41 @@
+package com.example.uartek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MenuActivity extends AppCompatActivity {
+    public String user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+    public void add_word_section(View view){
+        Intent intent = new Intent(MenuActivity.this,AddWordActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void words_button(View view){
+        Intent intent = new Intent(MenuActivity.this,WordsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void enter_the_exam_button(View view){
+        Intent intent = new Intent(MenuActivity.this, QuizActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void settings(View view){
+        Intent intent = new Intent(MenuActivity.this,SettingsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
